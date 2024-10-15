4f234325de071780d04b4bbf0b5bc158b214c1d1
Malik Abo Shabab <malikabushabab@gmail.com>
2024-10-15 17:25:47 +0300

    update

--- custom_addons/wk_backup_restore/__manifest__.py
+++ custom_addons/wk_backup_restore/__manifest__.py
@@ -1,18 +1,3 @@
-# -*- coding: utf-8 -*-
-#################################################################################
-# Author      : Webkul Software Pvt. Ltd. (<https://webkul.com/>)
-# Copyright(c): 2015-Present Webkul Software Pvt. Ltd.
-# All Rights Reserved.
-#
-#
-#
-# This program is copyright property of the author mentioned above.
-# You can`t redistribute it and/or modify it.
-#
-#
-# You should have received a copy of the License along with this program.
-# If not, see <https://store.webkul.com/license.html/>
-#################################################################################
 {
   "name"                 :  "Odoo Database Backup",
   "summary"              :  """Module provide feature to admin to take backups of his instance's database and later download them.""",  
