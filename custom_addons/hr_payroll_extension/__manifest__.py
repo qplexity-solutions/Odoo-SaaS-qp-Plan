3ec73fdb2a9de4d04c37d0b68ffe8ba0d8fa2ccb
tamoconsulting <30894232+tali83@users.noreply.github.com>
2024-12-18 18:54:17 +0100

    update

--- custom_addons/hr_payroll_extension/__manifest__.py
+++ custom_addons/hr_payroll_extension/__manifest__.py
@@ -15,13 +15,11 @@
     'website': 'https://www.qplexity.com',
     'license': 'LGPL-3',
     'category': 'Human Resources',
-    'version': '16.24.12.',
+    'version': '16.0.0.24',
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
-    'version': '16.0.0.23',
 
     # any module necessary for this one to work correctly
     'depends': ['base','hr','hr_holidays_attendance'],
