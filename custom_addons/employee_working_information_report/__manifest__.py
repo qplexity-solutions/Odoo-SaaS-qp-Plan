8ca0fa539cc2e0b7fd1ab963c7b11d552ae0f064
tamoconsulting <30894232+tali83@users.noreply.github.com>
2025-01-16 19:31:27 +0100

    descriptions

--- custom_addons/employee_working_information_report/__manifest__.py
+++ custom_addons/employee_working_information_report/__manifest__.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 {
-    'name': 'HR Employee Working Information Report in Odoo',
-    'version': '0.1',
+    'name': 'HR Employee Working Information Report',
+    'summary': 'HR Employee Working Information Report',
+    'description': """HR Employee Working Information Report""",
+    'author': 'qplexity solutions',
+    'website': 'https://www.qplexity.com',
+    'license': 'LGPL-3',
     'category': 'Human Resources',
-    'summary': 'HR Employee Working Information Report in Odoo',
-    'description': """HR Employee Working Information Report in Odoo""",
-    'author': "Siddiq Chauhdry | MarkhorTech",
-    "website": "https://www.markhortech.pk",
+    'version': '16.24.12.',
     'depends': ['base', 'hr', 'hr_holidays', 'hr_attendance'],
     'data': [
         'security/ir.model.access.csv',
